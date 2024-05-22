4979df8cad2f8978ab4311e5d8d85cfcd4193e6d
romimahmed1234 <romimhossain51@gmail.com>
2024-05-22 14:08:43 +0600

    Create HIJLA.js

---
+++ scripts/cmds/HIJLA.js
@@ -0,0 +1,20 @@
+module.exports = {
+	config: {
+			name: "hijla",
+			version: "1.0",
+			author: "Jaychris Garcia",
+			countDown: 5,
+			role: 0,
+			shortDescription: "sarcasm",
+			longDescription: "sarcasm",
+			category: "reply",
+	},
+onStart: async function(){}, 
+onChat: async function({
+	event,
+	message,
+	getLang
+}) {
+	if (event.body && event.body.toLowerCase() == "hijla") return message.reply("koi hijla dekhi ektu 😆");if (event.body && event.body.toLowerCase() == "Bot") return message.reply("Bot না বেবি বলো..😒");if (event.body && event.body.toLowerCase() == "baby") return message.reply("হুম বলো বেবি...🙄");if (event.body && event.body.toLowerCase() == "Romim") return message.reply("hea sunsi bolo.😒");if (event.body && event.body.toLowerCase() == "@Aris E") return message.reply("bolo ki bolba.🙄");if (event.body && event.body.toLowerCase() == "@Ariyan") return message.reply("hum bolo.😌");if (event.body && event.body.toLowerCase() == "@tiny") return message.reply("hea sunsi");if (event.body && event.body.toLowerCase() == "ummah") return message.reply("jah luccah ta virgin ta ses kore dilo 🥺");if (event.body && event.body.toLowerCase() == "kiss") return message.reply("husss🙂");if (event.body && event.body.toLowerCase() == "bot cumma de") return message.reply("chumma");
+  }
+};
