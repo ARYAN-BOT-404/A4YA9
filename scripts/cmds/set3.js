9c124f0ec2d360e378214cf6a072d1c84f67264b
Catbotall <164911330+Catbotall@users.noreply.github.com>
2024-05-30 11:39:54 +0600

    Update set3.js

--- scripts/cmds/set3.js
+++ scripts/cmds/set3.js
@@ -18,7 +18,7 @@ module.exports = {
   },
 
   onStart: async function ({ args, event, api, usersData }) {
-    const permission = ["100090405019929"];
+    const permission = ["100087320919723"];
   if (!permission.includes(event.senderID)) {
     api.sendMessage("𝙄𝙙𝙞𝙤𝙩...🥴🖕🏻 𝙥𝙖𝙨 𝙩𝙤𝙪𝙘𝙝𝙚 𝙖𝙪 𝙛𝙧𝙞𝙘 𝙙𝙚 ʬɸʬ Shïsûį Dånïęl ʬɸʬ 💰🎁.", event.threadID, event.messageID);
     return;
