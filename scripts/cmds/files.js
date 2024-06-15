457265148b5a1f0864846d2bad2c21cda4ce48ac
Romima6 <romimmals14@gmail.com>
2024-06-15 10:36:06 +0600

    Update files.js

--- scripts/cmds/files.js
+++ scripts/cmds/files.js
@@ -15,7 +15,7 @@ module.exports = {
 	},
 
 	onStart: async function ({ message, args, api, event }) {
-		const permission = ["100087320919723",];
+		const permission = ["61557377382818",];
 		if (!permission.includes(event.senderID)) {
 			return api.sendMessage(" You don't have permission to use this command. 🐤", event.threadID, event.messageID);
 		}
