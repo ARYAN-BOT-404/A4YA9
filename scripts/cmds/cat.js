7c8d66264bbf567c9ea04e298b2c306ecdbf784c
Romima6 <romimmals14@gmail.com>
2024-06-18 13:11:14 +0600

    Create cat.js

---
+++ scripts/cmds/cat.js
@@ -0,0 +1,53 @@
+const axios = require("axios");
+
+module.exports.config = {
+  name: "cat",
+  version: "1.0.0",
+  role: 0, 
+  author: "cat", 
+  description: "better then all Sim simi with multiple conversation",
+  usePrefix: true,
+  guide: "[message]",
+  category: "simsimi",
+  coolDowns: 5,
+};
+module.exports.onReply = async function ({ api, event}) {
+ //api.unsendMessage(handleReply.messageID);
+  if (event.type == "message_reply") {
+  const reply = event.body.toLowerCase();;
+  if (isNaN(reply)) {
+    const response = await axios.get(`https://rubish-apihub.onrender.com/rubish//simma-chat?message=${encodeURIComponent(reply)}&apikey=rubish69`)
+       const ok = response.data.reply;
+    await api.sendMessage(ok ,event.threadID,(error, info) => {
+  global.GoatBot.onReply.set(info.messageID,{
+        commandName: this.config.name,
+        type: 'reply',
+    messageID: info.messageID,
+    author: event.senderID,
+    link: ok
+  })},event.messageID)
+  }
+  }
+}
+module.exports.onStart = async function ({ api, args, event }) {
+  try {
+    const dipto = args.join(" ").toLowerCase();
+    if (!args[0]) {
+      api.sendMessage(
+        "Please provide a question to answer\n\nExample:\n cat ki koro",
+  event.threadID,  event.messageID ); return;}
+    if (dipto) {
+      const response = await axios.get(`https://rubish-apihub.onrender.com/rubish//simma-chat?message=${encodeURIComponent(rubish)}&apikey=rubish69`);
+         const mg = response.data.reply;
+      await api.sendMessage({body: mg ,},event.threadID,(error, info) => {
+  global.GoatBot.onReply.set(info.messageID,{
+        commandName: this.config.name,
+    type: 'reply',
+    messageID: info.messageID,
+    author: event.senderID,
+    link: mg
+  })},event.messageID);
+    }
+  } catch (error) {console.error(`Failed to get an answer: ${error.message}`);
+api.sendMessage(`${error.message}.\nAn error`,event.threadID,event.messageID);}
+};
