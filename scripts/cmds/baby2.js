1b7315c968858e9861c29b317423a20074ccebe9
Romima6 <romimmals14@gmail.com>
2024-07-08 16:40:25 +0600

    Update baby2.js

--- scripts/cmds/baby2.js
+++ scripts/cmds/baby2.js
@@ -1,11 +1,12 @@
- module.exports.config = {
-  name: "bby2",
+module.exports.config = {
+  name: "bby",
     author:"Romim",
     version: "72782828281919292992",
-  category: "text"
+category: "text"
 
 }
-module.exports.onStart = async function ({api,event,args}) {
+module.exports.onStart = async function ({}) { }
+module.exports.onChat = async function ({api,event,args}) {
   const fuck = args.join(" ")
   const axios = require("axios")
   const res =  await axios.get(`https://www.noobs-api.000.pe/dipto/baby?text=${fuck}`)
@@ -21,9 +22,9 @@ global.GoatBot.onReply.set(info.messageID, {commandName: this.config.name,
 }
 module.exports.onReply = async ({api,event,args}) => {
   const n = event.body.toLowerCase()
-  if (n.includes(bby)||n.includes(baby)||n.includes(A6)) {
-    const fuck = args.join(" ")
+  if (n.includes("bby")||n.includes("baby")||n.includes("A6")) {
     const axios = require("axios")
+      const fuck = args.join(" ")
     const res =  await axios.get(`https://www.noobs-api.000.pe/dipto/baby?text=${fuck}`)
     const response = res.data.reply 
    await api.sendMessage(`${response}`,event.threadID, (error,info) => {
