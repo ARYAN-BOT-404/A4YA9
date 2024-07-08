a3d019614091bf4d35b7c09fe641a3a6ba527156
Romima6 <romimmals14@gmail.com>
2024-07-08 21:16:13 +0600

    Create Ss.js

---
+++ scripts/cmds/Ss.js
@@ -0,0 +1,26 @@
+module.exports.config = {
+  name: "ss",
+  version: "1.0",
+  author: "Dipto",
+  role: 2,
+  description: "Take a screenshot of a website",
+  category: "utility",
+  guide: {en:"screenshot [URL]"},
+  coolDowns: 5
+};
+exports.onStart= async function ({api, event, args }) {
+  const url = args.join(" "); 
+  if (!url) {
+    return api.sendMessage('Please provide a URL.', event.threadID);
+  }
+  try {
+    api.sendMessage(
+      { body: "Screenshot Saved <😽", attachment: await global.utils.getStreamFromURL(`${global.GoatBot.config.api}/ss?url=${url}`)}, 
+      event.threadID,
+      event.messageID
+    );
+  } catch (error) {
+    console.error('Error taking screenshot:', error);
+    api.sendMessage('Failed to take a screenshot.', event.threadID);
+  }
+};
