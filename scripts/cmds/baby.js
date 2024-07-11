96d8e3b9c5346daaab9380d3ea08b01695428fab
Romima6 <romimmals14@gmail.com>
2024-07-11 09:09:35 +0600

    Update baby.js

--- scripts/cmds/baby.js
+++ scripts/cmds/baby.js
@@ -16,7 +16,7 @@ module.exports.onReply = async function ({ api, event}) {
   if (event.type == "message_reply") {
   const reply = event.body.toLowerCase();;
   if (isNaN(reply)) {
-    const response = await axios.get(`https://nobs-api.onrender.com/dipto/baby?text=${encodeURIComponent(reply)}`)
+    const response = await axios.get(`https://www.noobs-api.000.pe/dipto/baby?text=${encodeURIComponent(reply)}`)
        const ok = response.data.reply;
     await api.sendMessage(ok ,event.threadID,(error, info) => {
   global.GoatBot.onReply.set(info.messageID,{
@@ -37,7 +37,7 @@ module.exports.onStart = async function ({ api, args, event }) {
         "Please provide a question to answer\n\nExample:\nbaby ki koro",
   event.threadID,  event.messageID ); return;}
     if (dipto) {
-      const response = await axios.get(`https://nobs-api.onrender.com/dipto/baby?text=${dipto}`);
+      const response = await axios.get(`https://www.noobs-api.000.pe/dipto/baby?text=${dipto}`);
          const mg = response.data.reply;
       await api.sendMessage({body: mg ,},event.threadID,(error, info) => {
   global.GoatBot.onReply.set(info.messageID,{
