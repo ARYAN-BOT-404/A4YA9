9f49bba05cfa918dca428108f2004c147f63f762
Romima6 <romimmals14@gmail.com>
2024-07-15 15:54:37 +0600

    Update Album.js

--- scripts/cmds/Album.js
+++ scripts/cmds/Album.js
@@ -1,97 +1,143 @@
-module.exports.config = {
+ module.exports.config = {
   name: "album",
   author: "A6Y",
   category: "video sender"
-}
+};
 
 module.exports.onStart = async ({ api, event, args }) => {
   if (!args[0]) {
-    const a6Y = "♚═══ ∰𝙰𝙻𝙱𝚄𝙼 𝚂𝚃𝙰𝚁𝚃 ☚ ═══♚\n\n∰⇨𝙰𝙻𝙱𝚄𝙼 𝙲𝚁𝙴𝙳𝙸𝚃 𝙱𝚈 𝚁𝙾𝙼𝙸𝙼ᬊᬁ \n\n ✇1 -  𝙵𝙾𝙾𝚃𝙱𝙰𝙻𝙻   𝚅𝙸𝙳𝙴𝙾 !\n  ✇2 -  𝙼𝙴𝚂𝚂𝙸 1 !\n ✇3 -  𝙽𝙴𝚈𝙼𝙰𝚁  !\n  ✇4 -  𝚁𝙾𝙽𝙰𝙻𝙳𝙾 !\n ✇5 -  𝙵𝚁𝙴𝙴 𝙵𝙸𝚁𝙴 𝙴𝙳𝙸𝚃𝚉 𝚅𝙸𝙳𝙴𝙾   !\n  ✇6 -  𝙵𝚁𝙴𝙴 𝙵𝙸𝚁𝙴 𝙴𝙳𝙸𝚃𝚉 𝚂𝙰𝙳 𝚅𝙸𝙳𝙴𝙾 !\n ✇7 -  𝚆𝙷𝙸𝚃𝙴 01 !\n  ✇8 -  𝙱𝙾𝙽𝙴𝚇4 𝙵𝙵 !\n\n ✇9 -  𝙼1𝙽𝚇 !\n ✇11 - 𝙷𝙰𝚉𝚈 𝙼𝙰𝙽 !\n ✇12 - 𝙸𝚂𝙻𝙰𝙼𝙸𝙲 𝚅𝙸𝙳𝙴𝙾 !\n\n ✇13 - 𝙰𝙳𝙽𝙰𝙽 𝙷𝚄𝙹𝙾𝚁 𝚅𝙸𝙳𝙴𝙾 !\n ✇14  -  𝙻𝙾𝚅𝙴 𝚅𝙸𝙳𝙴𝙾 !\n✇15  - 𝚂𝚃𝙰𝚃𝚄𝚂 𝚅𝙸𝙳𝙴𝙾 \n ✇16  -  18+- 𝙷𝙾𝚁𝙽𝚈 𝚅𝙸𝙳𝙴𝙾 !\n ⚚━━━━━━━━━━━━━━━━━━━⚚\n★❦𝙴𝙽𝙹𝙾𝚈 𝚁4𝙼1𝙼 𝙰𝙻𝙱𝚄𝙼༒\n\n please enter a valid number of album"
-await api.sendMessage(a6Y,event.threadID,
+    api.setMessageReaction("🐸", event.messageID, (err) => {}, true);
+
+    const a6Y1 = "♚═══ ∰𝙰𝙻𝙱𝚄𝙼 𝚂𝚃𝙰𝚁𝚃 ☚ ═══♚\n\n∰⇨𝙰𝙻𝙱𝚄𝙼 𝙲𝚁𝙴𝙳𝙸𝚃 𝙱𝚈 𝚁𝙾𝙼𝙸𝙼ᬊᬁ \n\n ✇1 -  𝙵𝙾𝙾𝚃𝙱𝙰𝙻𝙻   𝚅𝙸𝙳𝙴𝙾 !\n  ✇2 -  𝙼𝙴𝚂𝚂𝙸 1 !\n ✇3 -  𝙽𝙴𝚈𝙼𝙰𝚁  !\n  ✇4 -  𝚁𝙾𝙽𝙰𝙻𝙳𝙾 !\n ✇5 -  𝙵𝚁𝙴𝙴 𝙵𝙸𝚁𝙴 𝙴𝙳𝙸𝚃𝚉 𝚅𝙸𝙳𝙴𝙾   !\n  ✇6 -  𝙵𝚁𝙴𝙴 𝙵𝙸𝚁𝙴 𝙴𝙳𝙸𝚃𝚉 𝚂𝙰𝙳 𝚅𝙸𝙳𝙴𝙾 !\n ✇7 -  𝚆𝙷𝙸𝚃𝙴 01 !\n  ✇8 -  𝙱𝙾𝙽𝙴𝚇4 𝙵𝙵 !\n\n ✇9 -  𝙼1𝙽𝚇 !\n ✇11 - 𝙷𝙰𝚉𝚈 𝙼𝙰𝙽 !\n ✇12 - 𝙸𝚂𝙻𝙰𝙼𝙸𝙲 𝚅𝙸𝙳𝙴𝙾 !\n\n ✇13 - 𝙰𝙳𝙽𝙰𝙽 𝙷𝚄𝙹𝙾𝚁 𝚅𝙸𝙳𝙴𝙾 !\n ✇14  -  𝙻𝙾𝚅𝙴 𝚅𝙸𝙳𝙴𝙾 !\n✇15  - 𝚂𝚃𝙰𝚃𝚄𝚂 𝚅𝙸𝙳𝙴𝙾 \n ✇16  -  18+- 𝙷𝙾𝚁𝙽𝚈 𝚅𝙸𝙳𝙴𝙾 !\n ⚚━━━━━━━━━━━━━━━━━━━⚚\n★❦𝙴𝙽𝙹𝙾𝚈 𝚁4𝙼1𝙼 𝙰𝙻𝙱𝚄𝙼༒\n\n please enter a valid number of album";
+    await api.sendMessage(a6Y1, event.threadID,
       (error, info) => {
         global.GoatBot.onReply.set(info.messageID, {
           commandName: this.config.name,
           type: "reply",
           messageID: info.messageID,
           author: event.senderID,
-          msg: a6Y,
+          msg: a6Y1,
+        });
+      },
+      event.messageID
+    );
+  } else if (args[0] === "2") {
+    api.setMessageReaction("😌", event.messageID, (err) => {}, true);
+
+    const a6Y2 = "♚═══ ∰𝙰𝙻𝙱𝚄𝙼 2 𝚂𝚃𝙰𝚁𝚃 ☚ ═══♚\n\n∰⇨𝙰𝙻𝙱𝚄𝙼 2 𝙲𝚁𝙴𝙳𝙸𝚃 𝙱𝚈 𝚁𝙾𝙼𝙸𝙼ᬊᬁ \n\n ✇17 -  𝙻𝚈𝚁𝙸𝙲𝚂 𝚅𝙸𝙳𝙴𝙾 !\n  ✇18 - 𝙰𝙴𝚂𝚃𝙷𝙴𝚃𝙸𝙲 𝚅𝙸𝙳𝙴𝙾  !\n ✇19 -  𝙲𝙰𝚁'𝚂 𝚅𝙸𝙳𝙴𝙾 !\n  ✇20-  𝙰𝙽𝙸𝙼𝙴 𝚅𝙸𝙳𝙴𝙾 !\n ✇21-  𝚂𝙾𝙻𝙾 𝙻𝙴𝚅𝙴𝙻𝙸𝙽𝙶 𝚅𝙸𝙳𝙴𝙾 !\n  ✇22 -  𝙼𝙰𝙳𝙰𝚁𝙰 𝚅𝙸𝙳𝙴𝙾 !\n ✇23 -  𝙽𝙰𝚁𝚄𝚃𝙾 𝚅𝙸𝙳𝙴𝙾 !\n✇24 -  𝙿𝙷𝙾𝙽𝙺 𝚅𝙸𝙳𝙴𝙾 !\n✇25 -  𝙶𝙸𝚁𝙻 𝚅𝙸𝙳𝙴𝙾 ! \n\n ⚚━━━━━━━━━━━━━━━━━━━⚚\n★❦𝙴𝙽𝙹𝙾𝚈 𝚁4𝙼1𝙼 𝙰𝙻𝙱𝚄𝙼༒\n\n please enter a valid number of album";
+    await api.sendMessage(a6Y2, event.threadID,
+      (error, info) => {
+        global.GoatBot.onReply.set(info.messageID, {
+          commandName: this.config.name,
+          type: "reply",
+          messageID: info.messageID,
+          author: event.senderID,
+          msg: a6Y2,
         });
       },
       event.messageID
     );
   }
-}
+};
 
 module.exports.onReply = async ({ api, event, Reply }) => {
   const axios = require("axios");
   try {
     if (event.type === "message_reply") {
-      const a6 = parseInt(event.body);
+      const a61 = parseInt(event.body);
       api.unsendMessage(Reply.messageID);
-      var r4m1m;
+      let r4m1m1;
 
-      switch (a6) {
+      switch (a61) {
         case 1:
-          r4m1m = "https://a6-video-api-t0il.onrender.com/video/football";
+          r4m1m1 = "${global.GoatBot.config.a6y}/football";
           break;
         case 2:
-          r4m1m = "https://a6-video-api-t0il.onrender.com/video/messi";
+          r4m1m1 = "${global.GoatBot.config.a6y}/messi";
           break;
         case 3:
-          r4m1m = "https://a6-video-api-t0il.onrender.com/video/Neymar";
+          r4m1m1 = "${global.GoatBot.config.a6y}/neymar";
           break;
         case 4:
-          r4m1m = "https://a6-video-api-t0il.onrender.com/video/cr7";
+          r4m1m1 = "${global.GoatBot.config.a6y}/cr7";
           break;
         case 5:
-          r4m1m = "https://a6-video-api-t0il.onrender.com/video/editff";
+          r4m1m1 = "${global.GoatBot.config.a6y}/editff";
           break;
         case 6:
-          r4m1m = "https://a6-video-api-t0il.onrender.com/video/sadff";
+          r4m1m1 = "${global.GoatBot.config.a6y}/sadff";
           break;
         case 7:
-          r4m1m = "https://a6-video-api-t0il.onrender.com/video/white01";
+          r4m1m1 = "${global.GoatBot.config.a6y}/white01";
           break;
         case 8:
-          r4m1m = "https://a6-video-api-t0il.onrender.com/video/ff";
+          r4m1m1 = "${global.GoatBot.config.a6y}/ff";
           break;
         case 9:
-          r4m1m = "https://a6-video-api-t0il.onrender.com/video/m1nx";
+          r4m1m1 = "${global.GoatBot.config.a6y}/m1nx";
           break;
         case 10:
-          r4m1m = "https://a6-video-api-t0il.onrender.com/video/sakib";
+          r4m1m1 = "${global.GoatBot.config.a6y}/sakib";
           break;
         case 11:
-          r4m1m = "https://a6-video-api-t0il.onrender.com/video/sigma";
+          r4m1m1 = "${global.GoatBot.config.a6y}/sigma";
           break;
         case 12:
-          r4m1m = "https://a6-video-api-t0il.onrender.com/video/islamic";
+          r4m1m1 = "${global.GoatBot.config.a6y}/islamic";
           break;
         case 13:
-          r4m1m = "https://a6-video-api-t0il.onrender.com/video/adnan";
+          r4m1m1 = "${global.GoatBot.config.a6y}/adnan";
           break;
         case 14:
-          r4m1m = "https://a6-video-api-t0il.onrender.com/video/love";
+          r4m1m1 = "${global.GoatBot.config.a6y}/love";
           break;
         case 15:
-          r4m1m = "https://a6-video-api-t0il.onrender.com/video/status";
+          r4m1m1 = "${global.GoatBot.config.a6y}/status";
           break;
         case 16:
-          r4m1m = "https://a6-video-api-t0il.onrender.com/video/horny";
+          r4m1m1 = "${global.GoatBot.config.a6y}/horny";
           break;
+case 17:
+          r4m1m1 = "${global.GoatBot.config.a6y}/lyrics";
+          break;
+case 18:
+          r4m1m1 = "${global.GoatBot.config.a6y}/aesthetic";
+          break;
+case 19:
+          r4m1m1 = "${global.GoatBot.config.a6y}/car";
+          break;
+case 20:
+          r4m1m1 = "${global.GoatBot.config.a6y}/anime";
+          break;
+case 21:
+          r4m1m1 = "${global.GoatBot.config.a6y}/arise";
+          break;
+case 22:
+          r4m1m1 = "${global.GoatBot.config.a6y}/madara";
+          break;
+case 23:
+          r4m1m1 = "${global.GoatBot.config.a6y}/naruto";
+          break;
+case 25:
+          r4m1m1 = "${global.GoatBot.config.a6y}/phonk";
+          break;
+case 26:
+          r4m1m1 = "${global.GoatBot.config.a6y}/girl";
+          break;
+
         default:
           api.sendMessage("Invalid selection. Please enter a valid number from the album.", event.threadID, event.messageID);
           return;
       }
 
-      const res = await axios.get(r4m1m);
-      const url = res.data.data;
-      const video = await axios.get(url, { responseType: 'stream' });
-        const uri = res.data.count;
-      const rm = video.data;
-      api.sendMessage({ body: `𝚂𝚄𝙲𝙲𝙴𝚂𝚂𝙵𝚄𝙻𝙻𝚈 𝙰6𝚈 𝙰𝙿𝙸 𝚂𝙴𝙽𝙳 𝚈𝙾𝚄𝚁 𝙰𝙻𝙱𝚄𝙼 𝚅𝙸𝙳𝙴𝙾  𝙰𝙽𝙳 𝚅𝙸𝙵𝙴𝙾 𝙲𝙾𝚄𝙽𝚃 [ ${uri} ]`, attachment: rm }, event.threadID, event.messageID);
+      const resp = await axios.get(r4m1m1);
+      const url1 = resp.data.data;
+      const video1 = await axios.get(url1, { responseType: 'stream' });
+      const uri1 = resp.data.count;
+      const rm1 = video1.data;
+      api.sendMessage({ body: `𝚂𝚄𝙲𝙲𝙴𝚂𝚂𝙵𝚄𝙻𝙻𝚈 𝙰6𝚈 𝙰𝙿𝙸 𝚂𝙴𝙽𝙳 𝚈𝙾𝚄𝚁 𝙰𝙻𝙱𝚄𝙼 𝚅𝙸𝙳𝙴𝙾  𝙰𝙽𝙳 𝚅𝙸𝙵𝙴𝙾 𝙲𝙾𝚄𝙽𝚃 [ ${uri1} ]`, attachment: rm1 }, event.threadID, event.messageID);
     }
   } catch (error) {
     api.sendMessage(`Error: ${error.message}`, event.threadID, event.messageID);
   }
-}
+};
