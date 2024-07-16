09cf5c87fcbe571b5afdbe0877e7ed1e54a4503c
Romima6 <romimmals14@gmail.com>
2024-07-16 14:33:47 +0600

    Update adduser.js

--- scripts/cmds/adduser.js
+++ scripts/cmds/adduser.js
@@ -4,11 +4,15 @@ const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 module.exports = {
 	config: {
 		name: "adduser",
-		version: "1.5",
+		version: "1.4",
 		author: "NTKhang",
 		countDown: 5,
 		role: 1,
-		description: {
+		shortDescription: {
+			vi: "Thêm thành viên vào box chat",
+			en: "Add user to box chat"
+		},
+		longDescription: {
 			vi: "Thêm thành viên vào box chat của bạn",
 			en: "Add user to box chat of you"
 		},
