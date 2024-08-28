df858f3ea5d0fdb2ed6a6e096fa3008a0f79856f
ARYAN-SIMPLE-ROBOT <robotaryan@yahoo.com>
2024-08-28 20:16:53 +0600

    Create inbox.js

---
+++ scripts/cmds/inbox.js
@@ -0,0 +1,37 @@
+module.exports = {
+  config: {
+    name: "inbox",
+    aliases: ["inboxme", "in"],
+    version: "1.0",
+    author: "ARYAN",
+    countDown: 10,
+    role: 2,
+    shortDescription: {
+      en: ""
+    },
+    longDescription: {
+      en: ""
+    },
+    category: "fun",
+    guide: {
+      en: ""
+    }
+  },
+  langs: {
+    en: {
+      gg: ""
+    },
+    id: {
+      gg: ""
+    }
+  },
+  onStart: async function({ api, event, args, message }) {
+    try {
+      const query = encodeURIComponent(args.join(' '));
+      message.reply("☑️ |✦ 𝗦𝘂𝗰𝗰𝗲𝘀𝘀𝗳𝘂𝗹𝗹 𝗬𝗼𝘂𝗿 𝗜𝗻𝗯𝗼𝘅 𝗧𝗲𝘅𝘁\n𝗖𝗵𝗲𝗰𝗸 𝗜𝗻𝗯𝗼𝘅", event.threadID);
+      api.sendMessage("👀 |✦ 𝗛𝗲𝗹𝗹𝗼 𝗝𝗮𝗻", event.senderID);
+    } catch (error) {
+      console.error("Error bro: " + error);
+    }
+  }
+};
