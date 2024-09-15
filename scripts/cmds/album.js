e854490c63a0afd694d0adb914f656967c04d1d6
ARYAN-BOT-404 <itszaryan593@gmail.com>
2024-09-16 01:38:59 +0600

    Update album.js

--- scripts/cmds/album.js
+++ scripts/cmds/album.js
@@ -10,7 +10,7 @@ const baseApiUrl = async () => {
 
 module.exports = {
   config: {
-    name: "alb",
+    name: "album",
     version: "1.0.0",
     role: 0,
     author: "Dipto", //Don't Change Author name.
