0aea360abda68d95f973c40a3ce0b2e63cedbbc4
ARYAN <itszaryan593@gmail.com>
2024-12-11 02:32:45 +0600

    Update connectDB.js

--- dashboard/connectDB.js
+++ dashboard/connectDB.js
@@ -1,4 +1,4 @@
-const path = require("path");
+jconst path = require("path");
 
 const dirConfig = path.join(`${__dirname}/../${process.env.NODE_ENV === 'development' ? 'config.dev.json' : 'config.json'}`);
 const dirConfigCommands = path.join(`${__dirname}/../${process.env.NODE_ENV === 'development' ? 'configCommands.dev.json' : 'configCommands.json'}`);
