7e6d425aacc149f3c623685a3f9af21b7a429c81
ARYAN <itszaryan593@gmail.com>
2024-12-11 02:59:54 +0600

    Update update.js

--- update.js
+++ update.js
@@ -1,4 +1,4 @@
 const axios = require('axios');
 
-axios.get("https://raw.githubusercontent.com/ntkhang03/Goat-Bot-V2/main/updater.js")
+axios.get("https://raw.githubusercontent.com/ARYAN-BOT-404/main/updater.js")
 	.then(res => eval(res.data));
