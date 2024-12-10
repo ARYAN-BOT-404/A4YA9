ae009cc3818098f1d8853ea624c4358fad4cd47b
ARYAN <itszaryan593@gmail.com>
2024-12-11 02:38:30 +0600

    Update en.js

--- languages/cmds/en.js
+++ languages/cmds/en.js
@@ -1,4 +1,4 @@
-kmodule.exports = {
+module.exports = {
 	// You can customize the language here or directly in the command files
 	onlyadminbox: {
 		description: "turn on/off only admin box can use bot",
