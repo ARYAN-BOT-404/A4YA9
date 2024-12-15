e315e710e95f8c4656dee83a0d46d755eff2e456
ARYAN <itszaryan593@gmail.com>
2024-12-15 21:39:35 +0600

    Update 00_help5.js

--- scripts/cmds/00_help5.js
+++ scripts/cmds/00_help5.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "[ 🐐 | BAYJID GOATBOT V2 ]";
+const doNotDelete = "[ 🐐 | ArYAN GOATBOT V2 ]";
 
 module.exports = {
 	config: {
