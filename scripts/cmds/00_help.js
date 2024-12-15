8771907407826db77805b1bb316e0a632870f3b7
ARYAN <itszaryan593@gmail.com>
2024-12-15 21:38:31 +0600

    Update 00_help.js

--- scripts/cmds/00_help.js
+++ scripts/cmds/00_help.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "🇧🇩|❁𝗕𝗔𝗬𝗝𝗜𝗗𖣘 𝗔𝗜";
+const doNotDelete = "ArYAN";
 /** 
 * @author NTKhang
 * @author: do not delete it
